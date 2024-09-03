e92dde45cbb1949643d9040bb1b75059e4c2a86e
premchandellendula <premcellendula@gmail.com>
2024-09-03 21:37:31 +0530

    gallery loader

--- frontend/src/pages/gallery/Gallery.jsx
+++ frontend/src/pages/gallery/Gallery.jsx
@@ -3,26 +3,35 @@ import Appbar from '../../components/appbar/Appbar'
 import Footer from '../../components/footer/Footer'
 import GalleryCard from '../../components/gallery/GalleryCard'
 
-import ringImg1 from '../../images/gallery/ring1.jpg'
-import ringImg2 from '../../images/gallery/ring2.jpg'
-import bangleImg1 from '../../images/gallery/banlge1.jpg'
-import bangleImg2 from '../../images/gallery/bangle2.jpg'
-import braceletImg1 from '../../images/gallery/bracelet1.jpg'
-import braceletImg2 from '../../images/gallery/bracelet2.jpg'
-import necklaceImg1 from '../../images/gallery/necklace1.jpg'
-import necklaceImg2 from '../../images/gallery/necklace2.jpg'
-import necklaceImg3 from '../../images/gallery/necklace3.jpg'
 import axios from 'axios'
+import ProductSkeleton from '../../components/loaders/ProductSkeleton'
 
 const Gallery = () => {
   const [gallery, setGallery] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios.get('http://localhost:3000/api/v1/gallery')
       .then(res => {
         setGallery(res.data.products)
+        setLoading(false)
       })
   }, []);
+
+  if(loading){
+    return <div>
+      <Appbar />
+        <div className='grid grid-cols-3 w-[85%] m-auto gap-y-10 my-20'>
+          <ProductSkeleton />
+          <ProductSkeleton />
+          <ProductSkeleton />
+          <ProductSkeleton />
+          <ProductSkeleton />
+          <ProductSkeleton />
+        </div>
+      <Footer />
+    </div>
+  }
   return (
     <div>
       <Appbar />
